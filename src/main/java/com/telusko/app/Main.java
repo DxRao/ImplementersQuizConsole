018cba0278e2559b1e8c60a822adf9d6e726431e
D Rao <tdayakarrao@gmail.com>
2023-10-20 04:36:04 +0530

    commit

--- src/main/java/com/telusko/app/Main.java
+++ src/main/java/com/telusko/app/Main.java
@@ -3,6 +3,7 @@ package com.telusko.app;
 import com.telusko.app.QuizService;
 
 public class Main {
+	
     public static void main(String[] args) {
 
         QuizService service = new QuizService();
